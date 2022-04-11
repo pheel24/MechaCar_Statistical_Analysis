17e7f7c9178be56f51f1d202fb93a8942c261ee2
Philip Kirwan <kirwanpm@gmail.com>
2022-04-10 22:00:46 -0600

    adding summary stats

--- MechaCar_Challenge.R
+++ MechaCar_Challenge.R
@@ -1,6 +1,7 @@
 #Dependencies
 library(dplyr)
 
+# D1
 #read in data
 MechaCar_mpg<-read.csv("MechaCar_mpg.csv",check.names = F,stringsAsFactors = F)
 
@@ -12,4 +13,15 @@ linear_model<-lm(mpg~vehicle_length+vehicle_weight+
 #alpha and r^2
 summary(linear_model)
 # r^2= 0.71, p= 5.35e-11
+
+
+#D2
+#read data
+Suspension_coil_data<-read.table("Suspension_Coil.csv",check.names = F,stringsAsFactors = F)
+
+# total summary
+total_summary<-Suspension_coil_data %>% summarize(mean_PSI=mean(PSI),median_PSI=median(PSI),variance_PSI=var(PSI),sd_PSI=sd(PSI))
+
+#lot summary
+lot_summary<-Suspension_coil_data %>% group_by(Manufacturing_Lot) %>% summarize(mean_PSI=mean(PSI),median_PSI=median(PSI),variance_PSI=var(PSI),sd_PSI=sd(PSI), .groups='keep')
 
